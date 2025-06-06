01409fe708049a3e21f40ee40e4803221c76003f
domnichds <domni4.d2@yandex.ru>
2025-06-07 00:00:15 +0300

    fix tests

--- tests/test_weatherdata.cpp
+++ tests/test_weatherdata.cpp
@@ -1,12 +1,15 @@
 #include "weatherdata.h"
 #include <cassert>
+#include <cmath>
+
+#define APPROX_EQUAL(a, b) (std::fabs((a)-(b)) < 1e-4)
 
 class TestObs : public Observer {
 public:
     float t = 0, h = 0, p = 0;
-void Update(float temp, float hum, float pres) override {
-    t = temp; h = hum; p = pres;
-}
+    void Update(float temp, float hum, float pres) override {
+        t = temp; h = hum; p = pres;
+    }
 };
 
 int main() {
@@ -14,10 +17,10 @@ int main() {
     TestObs o;
     w.RegisterObserver(o);
     w.SetMeasurement(4.2, 5.1, 9.9);
-    assert(o.t == 4.2 && o.h == 5.1 && o.p == 9.9);
+    assert(APPROX_EQUAL(o.t, 4.2) && APPROX_EQUAL(o.h, 5.1) && APPROX_EQUAL(o.p, 9.9));
     w.RemoveObserver(o);
     o.t = 0; o.h = 0; o.p = 0;
     w.SetMeasurement(1,1,1);
-    assert(o.t == 0 && o.h == 0 && o.p == 0);
+    assert(APPROX_EQUAL(o.t, 0) && APPROX_EQUAL(o.h, 0) && APPROX_EQUAL(o.p, 0));
     return 0;
 }
